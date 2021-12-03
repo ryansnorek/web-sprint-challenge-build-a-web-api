1c602b7539a6674694927d933d68f842b46e62c4
Ryan <snorekr@gmail.com>
2021-12-03 09:27:05 -0500

    connect project routes

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,9 @@
-// Write your "projects" router here!
+const express = require("express");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.json({ message: "hi" });
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const server = express();
 
-// Configure your server here
+const projectsURL = "/api/projects";
+const projectsRoutes = require("./projects/projects-router");
+
+server.use(projectsURL, projectsRoutes);
+server.use(express.json());
+
+
+
 // Build your actions router in /api/actions/actions-router.js
 // Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
 
 module.exports = server;
