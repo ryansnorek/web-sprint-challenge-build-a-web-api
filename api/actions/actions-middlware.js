3c69ec4e760c1a5bab6d6b8084bd21d7e0f4b8cd
Ryan <snorekr@gmail.com>
2021-12-03 12:37:37 -0500

    GET action by ID

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,13 @@
-// add middlewares here related to actions
+const Actions = require("./actions-model");
+
+async function validateActionID(req, res, next) {
+    try { req.action = await Actions.get(req.params.id) }
+    catch (e) { next(e) }
+    if (!req.action) res.status(404).json({ message: "Not found" });
+    next();
+}
+
+module.exports = {
+    validateActionID,
+
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const Actions = require("./actions-model");
+const {
+    validateActionID 
+ } = require("./actions-middlware");
 
 router.get("/", (req, res, next) => Actions.get().then(result => res.json(result)).catch(next));
+
+router.get("/:id", validateActionID, (req, res) => res.json(req.action));
+
 
 router.use((err, req, res, next) => {
     res.status(err.status || 500).json({
