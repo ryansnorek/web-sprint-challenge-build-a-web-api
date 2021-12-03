982fbf5024634bdee001f5dcddb5e96d4c84efc2
Ryan <snorekr@gmail.com>
2021-12-03 10:56:20 -0500

    GET project by id, middleware setup, validate project ID

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,14 @@
-// add middlewares here related to projects
+const Projects = require("./projects-model");
+
+async function validateID(req, res, next) {
+    try {
+        const project = await Projects.get(req.params.id);
+        if (!project) res.status(404).json({ message: "Not found" });
+        req.project = project;
+        next();
+    } catch (e) { next(e) }
+}
+
+module.exports = {
+    validateID,
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,14 +1,15 @@
 const express = require("express");
-
 const router = express.Router();
-
 const Projects = require("./projects-model");
+const { validateID } = require("./projects-middleware");
 
 router.get("/", (req, res, next) => {
     Projects.get()
         .then(result => res.json(result))
         .catch(next)
 });
+
+router.get("/:id", validateID, (req, res) => res.json(req.project));
 
 router.use((err, req, res, next) => {
     res.status(err.status || 500).json({
